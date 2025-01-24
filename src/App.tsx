819db3482c52cab1c01860fde582a58ac05c2792
Kittoff <lozano.christophe@protonmail.com>
2025-01-24 14:03:40 +0100

    feat(ticket_1): Added React Query package and setup the initialisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import Header from "./components/Header/Header.tsx";
 import { AppRoutes } from "./routes/index.tsx";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <AppRoutes />
-    </>
+    </QueryClientProvider>
   );
 };
 
